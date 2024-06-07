4b5e03c0a8b2e60ec754a49fa6c07ddd5cb6c880
BuildTools <unconfigured@null.spigotmc.org>
2024-06-07 19:50:16 +0800

    Added Skeleton, Completed Fetching, Removed Strict Mode, Allowed JS

--- src/components/CryptoBox.tsx
+++ src/components/CryptoBox.tsx
@@ -12,13 +12,16 @@ import {
     CommandInput,
     CommandItem,
     CommandList,
+    CommandLoading,
 } from "@/components/ui/command";
 import {
     Popover,
     PopoverContent,
     PopoverTrigger,
 } from "@/components/ui/popover";
-import { IonCol, IonGrid, IonImg, IonRow } from "@ionic/react";
+import { IonCol, IonGrid, IonImg, IonRow, IonSkeletonText } from "@ionic/react";
+import { cryptoFetch } from '../utils/market-price/index.js'
+
 
 const frameworks = [
     {
@@ -43,49 +46,89 @@ const frameworks = [
     },
 ];
 
-export function CryptoContent({...props}) {
+export function CryptoContent({ name, ticker, price, skeleton }: { name: string; ticker: string; price: string; skeleton: boolean; }) {
     return (
-        <IonGrid class="ion-no-padding" {...props}>
-            <IonRow class="ion-align-items-center ion-justify-content-start gap-3">
-                <IonCol size="auto">
-                    <IonImg
-                        class="w-11 h-auto"
-                        src="https://pdax-prod-mobile-ap-southeast-1.s3.ap-southeast-1.amazonaws.com/images/cryptos/btc.png"
-                    ></IonImg>
-                </IonCol>
-                <IonCol size="auto" class="space-y-1">
+        <>
+            {skeleton ?
+                (
+                    <IonGrid class="ion-no-padding">
+                        <IonRow class="ion-align-items-center ion-justify-content-start gap-3">
+                            <IonCol size="auto">
+                                <IonSkeletonText animated={true} className="w-[44px] h-[44px] rounded-full"></IonSkeletonText>
+                            </IonCol>
+                            <IonCol size="auto" class="space-y-1">
 
-                    <IonRow>
-                        <IonCol size="12" class="font-medium">Bitcoin (BTC)</IonCol>
-                    </IonRow>
-                    <IonRow>
-                        <IonCol size="12" class="font-mono text-xs">₱4,179,043.83</IonCol>
-                    </IonRow>
+                                <IonRow>
+                                    <IonSkeletonText animated={true} className="w-24 "></IonSkeletonText>
+                                </IonRow>
+                                <IonRow>
+                                    <IonSkeletonText animated={true} className="w-20 "></IonSkeletonText>
+                                </IonRow>
 
-                </IonCol>
-            </IonRow>
-        </IonGrid>
+                            </IonCol>
+                        </IonRow>
+                    </IonGrid>
+                )
+                : (
+                    <IonGrid class="ion-no-padding">
+                        <IonRow class="ion-align-items-center ion-justify-content-start gap-3">
+                            <IonCol size="auto">
+                                <IonImg
+                                    class="w-11 h-auto"
+                                    src={`https://pdax-prod-mobile-ap-southeast-1.s3.ap-southeast-1.amazonaws.com/images/cryptos/${ticker.toLowerCase()}.png`}
+                                ></IonImg>
+                            </IonCol>
+                            <IonCol size="auto" class="space-y-1">
+
+                                <IonRow>
+                                    <IonCol size="12" class="font-medium">{name} ({ticker})</IonCol>
+                                </IonRow>
+                                <IonRow>
+                                    <IonCol size="12" class="font-mono text-xs">₱{price}</IonCol>
+                                </IonRow>
+
+                            </IonCol>
+                        </IonRow>
+                    </IonGrid>
+                )}
+        </>
     )
+}
+
+interface Coin {
+    ask: number,
+    askQuotedAt: string,
+    bid: number,
+    bidQuotedAt: string,
+    currencyPair: string;
+    tradedCurrency: {
+        name: string;
+        ticker: string;
+    },
+    updatedAt: number;
 }
 
 export function ComboboxDemo() {
     const [isOpen, setOpen] = React.useState(false);
     const [value, setValue] = React.useState("");
+    const [loading, setLoading] = React.useState(true);
+    const [coins, setCoins] = React.useState<Coin[]>([]);
+    const times = Array.from({ length: 20 });
+    React.useEffect(() => {
+        const asyncRun = async () => {
+            const tokens = await cryptoFetch()
+            setCoins(tokens)
+            setLoading(false)
+            console.log('mapper', coins)
+        }
+        if (isOpen) {
+            asyncRun()
+        }
+    }, [isOpen]);
 
     return (
         <Popover open={isOpen} onOpenChange={setOpen}>
             <PopoverTrigger asChild>
-                {/* <Button
-                    variant="outline"
-                    role="combobox"
-                    aria-expanded={open}
-                    className="w-[200px] justify-between"
-                >
-                    {value
-                        ? frameworks.find((framework) => framework.value === value)?.label
-                        : "Select framework..."}
-                    <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                </Button> */}
                 <Button
                     size="lg"
                     className="w-full bg-white border-[8px] justify-between p-3 h-auto text-start"
@@ -94,39 +137,61 @@ export function ComboboxDemo() {
                     aria-expanded={open}
                 >
                     {value
-                        ? frameworks.find((framework) => framework.value === value)?.label
+                        ? (() => {
+                            const selected = coins.find((i) => i.tradedCurrency.name === value);
+                            return selected ? (
+                                <CryptoContent
+                                    name={selected.tradedCurrency.name}
+                                    ticker={selected.tradedCurrency.ticker}
+                                    price="N/A"
+                                />
+                            ) : null
+                        })()
                         :
-                        <CryptoContent />
+                        <CryptoContent name="N/A" ticker="N/A" price="N/A" skeleton={true} />
                     }
+
                     <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
                 </Button>
             </PopoverTrigger>
-            <PopoverContent className="w-[100vw] p-2">
+            <PopoverContent className="w-[100vw] p-2 pt-0">
                 <Command>
                     <CommandInput placeholder="Search Coin..." />
                     <CommandList>
-                        <CommandEmpty>No framework found.</CommandEmpty>
-                        <CommandGroup>
-                            {frameworks.map((framework) => (
-                                <CommandItem
-                                    key={framework.value}
-                                    value={framework.value}
-                                    onSelect={(currentValue) => {
-                                        setValue(currentValue === value ? "" : currentValue);
-                                        setOpen(false);
-                                    }}
-                                >
-                                    {/* {framework.label} */}
-                                    <CryptoContent/>
-                                    <CheckIcon
-                                        className={cn(
-                                            "ml-auto h-4 w-4",
-                                            value === framework.value ? "opacity-100" : "opacity-0"
-                                        )}
-                                    />
-                                </CommandItem>
-                            ))}
-                        </CommandGroup>
+                        {loading ? 
+                            <CommandGroup>
+                                {times.map((_, index) => (
+                                    <CommandItem key={index}><CryptoContent skeleton={true} /></CommandItem>
+                                ))}
+                            </CommandGroup> :
+                            <>
+                                <CommandEmpty>Asset not found.</CommandEmpty>
+                                <CommandGroup>
+                                    {coins.map((i) => {
+                                        return (
+                                            <CommandItem
+                                                key={i.currencyPair}
+                                                value={i.tradedCurrency.name}
+                                                keywords={[i.tradedCurrency.ticker]}
+                                                onSelect={(currentValue) => {
+                                                    setValue(currentValue === value ? "" : currentValue);
+                                                    setOpen(false);
+                                                }}
+                                            >
+                                                {/* {framework.label} */}
+                                                <CryptoContent name={i.tradedCurrency.name} ticker={i.tradedCurrency.ticker} price="N/A" />
+                                                <CheckIcon
+                                                    className={cn(
+                                                        "ml-auto h-4 w-4",
+                                                        value === i.tradedCurrency.name ? "opacity-100" : "opacity-0"
+                                                    )}
+                                                />
+                                            </CommandItem>
+                                        )
+                                    })}
+                                </CommandGroup>
+                            </>
+                        }
                     </CommandList>
                 </Command>
             </PopoverContent>
--- src/utils/market-price/index.js
+++ src/utils/market-price/index.js
@@ -1,16 +1,17 @@
-async function fetchApi() {
+export const cryptoFetch = async () => {
+    console.log('fetching crypto')
     let res = await fetch('https://services.pdax.ph/api/liquidity/otc/v1/marketprices')
     let data = await res.json()
 
-    let coin = data.result.find((i) => {
-        return i.currencyPair === 'BTC_PHP'
+    let coin = data.result.filter((i) => {
+        return !i.currencyPair.includes('BILL') && i.currencyPair !== ''
     })
-    let askBid = new BuySell(coin.ask, coin.bid)
-
-    return { coin, askBid }
+    // let askBid = new BuySell(coin.ask, coin.bid)
+    console.log('cryptoFetch', coin)
+    return coin
 }
 
-(async () => {
-    const { coin, askBid } = await fetchApi();
-    console.log(coin, askBid.buy(122));
-})();
+// (async () => {
+//     const { coin, askBid } = await fetchApi();
+//     console.log(coin, askBid.buy(122));
+// })();
